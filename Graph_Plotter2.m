e8d827f5304493cc7d02e0b5388983f0f8753ea2
SainathB <sain8339@gmail.com>
2014-02-25 17:42:23 +0530

    This matlab code reads data from file sample_data.txt and plots the Graph

---
+++ Graph_Plotter2.m
@@ -0,0 +1,19 @@
+file=fopen('sample_data.txt','r');
+y=fscanf(file,'%f');
+len=length(y);
+Wavelength=[];
+Transmission=[];
+j=1;
+l=1;
+for i=1:len
+    
+    if mod(i,2)
+       Wavelength(j)=y(i);
+       j=j+1;
+    else
+        Transmission(l)=y(i);
+        l=l+1;
+    end;    
+    
+end;
+plot(Wavelength,Transmission);
