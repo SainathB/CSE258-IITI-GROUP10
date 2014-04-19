6bf2bd807eec377ce188a2301aa382dd901c8efe
JuniperGujral <junipergujral@gmail.com>
2014-04-19 14:30:05 +0530

    debugged filter files

--- calc_reflectivity.m
+++ calc_reflectivity.m
@@ -8,9 +8,11 @@ function R_final= calc_reflectivity(d_store,n_store,alpha_store,ref_sub)
 %n_store,alpha_store and ref_sub are for a particular lambda
 I0=100;
 R_temp=[];
+It=0;
+Ir=0;
 x=numel(d_store);
 for i=1:x+1
-    if x==1
+    if i==1
         Ir=((n_store(i)-1)^2/(n_store(i)+1)^2)*I0;   %refractive index of air is taken as 1
         It=I0-Ir;
         It=It*exp(-1*alpha_store(i)*d_store(i));
@@ -20,7 +22,7 @@ for i=1:x+1
         It=It-Ir;
         R_temp(i)=Ir;
     else
-        Ir=((n_store(i)-n_store(i-1))^2/((n_store(i)+n_store(i-1))^2)*It;
+        Ir=((n_store(i)-n_store(i-1))^2/((n_store(i)+n_store(i-1))^2))*It;
         It=It-Ir;
         It=It*exp(-1*alpha_store(i)*d_store(i));
         R_temp(i)=Ir;
@@ -31,11 +33,15 @@ R=[];
 for j=1:R_num
     if j==1
         R(j)=R_temp(j);
+    elseif j==2
+        R(j)=R_temp(j);
+        R(j)=R(j)*exp(-1*alpha_store(1)*d_store(1));
+        R(j)=R(j)-((1-n_store(1))^2/(n_store(1)+1)^2)*R(j);
     else
         R(j)=R_temp(j);
-        for k=j:-1:2
-            R(j)=R(j)*exp(-1*alpha_store(k)*d_store(k));
-            R(j)=R(j)-((n_store(k-1)-n_store(k))^2/(n_store(k-1)+n_store(k))^2)*R(j);
+        for k=j:-1:3
+            R(j)=R(j)*exp(-1*alpha_store(k-1)*d_store(k-1));
+            R(j)=R(j)-((n_store(k-2)-n_store(k-1))^2/(n_store(k-2)+n_store(k-1))^2)*R(j);
         end
          R(j)=R(j)*exp(-1*alpha_store(1)*d_store(1));
          R(j)=R(j)-((1-n_store(1))^2/(1+n_store(1))^2)*R(j);
